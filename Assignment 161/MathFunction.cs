52d51b877d9304cf3b4107bde929c890b30f860c
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-08 19:59:01 -0700

    Add project files.

---
+++ Assignment 161/MathFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_161
+{
+    class MathFunction
+    {        
+        public void functionOne(int int1, int int2)
+        {       
+            int Total = int1 * 2;
+            Console.WriteLine(int2);  
+        }   
+    }
+}
---
+++ Assignment 161/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 12;
+            MathFunction function = new MathFunction();
+
+            function.functionOne(num1, num2);
+
+            function.functionOne(int1: num1, int2: num2);
+
+            Console.ReadLine();
+        }
+    }
+}
